f74f3b00567a25e2c2d41f8d90c7dc4cfd185364
Tahmid... <ttasin345@gmail.com>
2025-08-17 21:56:12 +0100

    Added Cookie Policy

--- js/main.js
+++ js/main.js
@@ -537,3 +537,33 @@ document.querySelectorAll('.course-modal-overlay').forEach(modal => {
   });
 });
 
+//Cookie-Policy
+
+SetCookie = (cName, cValue, expDays) => {
+  let date = new Date();
+  date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = cName + "=" + cValue + ";" + expires + "; path=/, path=/contact";
+}
+
+getCookie = (cName) => {
+  const name = cName + "=";
+  const cDecoded = decodedURIComponent(document.cookies);
+  const cArr = cDecoded.split("; ");
+  let value;
+  cArr.forEach(val => {
+    if(val.indexOf(name) === 0) value = val.substring(name.length);
+  })
+  return value;
+}
+document.querySelector("#cookies-btn").addEventListener("click",()=>{
+  document.querySelector("#cookies").style.display = "none";
+  setCookies("cookie", true, 1);
+})
+
+cookieMessage = () => {
+  if(!getCookie('cookie'))
+    document.querySelector("#cookies").style.display = "block";
+}
+
+window.addEventListener("load", cookieMessage);
